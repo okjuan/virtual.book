e14b289eccf6d00e65c3c3bc5ee3172c77264554
Juan <jcgallegdup@gmail.com>
2023-11-20 19:25:49 -0800

    (Incomplete) Implement backlinks

---
+++ _plugins/backlinks.rb
@@ -0,0 +1,24 @@
+module Jekyll
+  module Backlinks
+    def backlink(input)
+      backlinks = []
+      site = @context.registers[:site]
+      # TODO: figure out bug that causes the-road to link to itself instead of linking to the-road-movie
+      # TODO: figure out why it doens't link to both the-road AND the-road-movie
+      # TODO: figure out why live-in-the-moment doesn't backlink to like-you-life
+      # TODO: figure out why coordinate-metaphors doesn't backlink to four-thousand-weeks
+      site.posts.docs.each do |post|
+        if post&.content&.include? input
+          backlinks << "<a href=\"#{site.baseurl}#{post.url}\">#{post.data['title']}</a>"
+        end
+      end
+      if (backlinks.length > 0)
+        return "Mentioned in #{backlinks.join(', ')}"
+      else
+        return nil
+      end
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::Backlinks)
