6cadc666e91693ebf4fecdb3520eabc480e58b0d
Juan <jcgallegdup@gmail.com>
2023-11-20 21:56:23 -0800

    Prevent backlinks to self

--- _plugins/backlinks.rb
+++ _plugins/backlinks.rb
@@ -1,6 +1,6 @@
 module Jekyll
   module Backlinks
-    def backlink(input)
+    def backlink(post_url)
       backlinks = []
       site = @context.registers[:site]
       # TODO: figure out bug that causes the-road to link to itself instead of linking to the-road-movie
@@ -8,8 +8,10 @@ module Jekyll
       # TODO: figure out why live-in-the-moment doesn't backlink to like-you-life
       # TODO: figure out why coordinate-metaphors doesn't backlink to four-thousand-weeks
       site.posts.docs.each do |post|
-        if post&.content&.include? input
-          backlinks << "<a href=\"#{site.baseurl}#{post.url}\">#{post.data['title']}</a>"
+        if post&.content&.include? post_url
+          if post&.url != post_url
+            backlinks << "<a href=\"#{site.baseurl}#{post.url}\">#{post.data['title']}</a>"
+          end
         end
       end
       if (backlinks.length > 0)
