2bc9180e8864f1dcaf176d0ed660c1e4e5c5df6a
Juan <jcgallegdup@gmail.com>
2023-11-20 22:25:48 -0800

    Fix backlinks in cases where a post links to an older post

--- _plugins/backlinks.rb
+++ _plugins/backlinks.rb
@@ -1,15 +1,16 @@
 module Jekyll
   module Backlinks
-    def backlink(post_url)
+    def backlink(target_post)
       backlinks = []
+      # e.g. _posts/2022-02-02-dune.md => 2022-02-02-dune
+      post_file_name = File.basename(target_post.path,File.extname(target_post.path))
       site = @context.registers[:site]
-      # TODO: figure out bug that causes the-road to link to itself instead of linking to the-road-movie
-      # TODO: figure out why it doens't link to both the-road AND the-road-movie
-      # TODO: figure out why live-in-the-moment doesn't backlink to like-you-life
-      # TODO: figure out why coordinate-metaphors doesn't backlink to four-thousand-weeks
       site.posts.docs.each do |post|
-        if post&.content&.include? post_url
-          if post&.url != post_url
+        # file name matches when target_post is older than post and URL matches when target_post is newer
+        # this is (presumably) because posts are processed in chronological order, and so half are in raw form
+        # and the rest are in HTML form
+        if post&.content&.include?(post_file_name) || post&.content&.include?(target_post.url)
+          if post&.url != target_post.url
             backlinks << "<a href=\"#{site.baseurl}#{post.url}\">#{post.data['title']}</a>"
           end
         end
