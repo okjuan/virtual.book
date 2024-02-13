0c9c7d7b29b63aaa42e616029d33432993b50646
Juan <jcgallegdup@gmail.com>
2024-02-12 18:09:08 -0800

    Set up hover card on-hover skeleton

---
+++ _plugins/hoverCards.rb
@@ -0,0 +1,23 @@
+module Jekyll
+  class HoverCardTag < Liquid::Tag
+
+    def initialize(tag_name, input, tokens)
+      super
+      input_split = input.split("|")
+      @text = input_split[0].strip
+      @post_name = input_split[1]? input_split[1].strip : 'TODO'
+    end
+
+    def render(context)
+      site = context.registers[:site]
+      post = site.posts.docs.find { |p| File.basename(p.basename, ".md") == @post_name }
+      if post
+        "<span class='hover-link'><a href='#{site.baseurl}#{post.permalink}'>#{@text}</a><span class='hover-card'>TODO</span></span>"
+      else
+        puts "Post not found: @post_name=#{@post_name}"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('post_url_with_hover_card', Jekyll::HoverCardTag)
