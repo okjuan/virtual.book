af25c3b2ed13a513e7811af5b542e52a92a3d4e2
Juan <jcgallegdup@gmail.com>
2024-03-11 16:47:53 -0700

    Fix bug where hovercard content is raw for newer posts (relative to current post) Do so by waiting until entire site has been rendered before inserting hovercards to prevent issues where linked_post.content for newer posts are still raw Markdown

--- _plugins/hoverCards2.rb
+++ _plugins/hoverCards2.rb
@@ -4,15 +4,19 @@ module Jekyll
     priority :low
 
     def generate(site)
-      Jekyll::Hooks.register :posts, :post_render do |post|
-        post.output = post.output.gsub(/<a id="(.*?)" class="internal-site-link"(.*?)<\/a>/) do
-            id = $1
-            linked_post = site.posts.docs.find { |post| post.id == id }
-            title = linked_post.data['title']
-            content = linked_post.content
-            "</p><a class='hover-link' #{$2}</a><div class='hover-card-container'><div class='hover-card'><strong class='hover-card-title'>#{title}</strong><br>#{content}</div></div><p>"
-          end
+        # runs when entire site has been rendered, but before any files are written.
+        # this prevents issues where linked_post.content for newer posts are still raw Markdown
+        Jekyll::Hooks.register :site, :post_render do |site|
+            site.posts.docs.each do |post|
+                post.output = post.output.gsub(/<a id="(.*?)" class="internal-site-link"(.*?)<\/a>/) do
+                    id = $1
+                    linked_post = site.posts.docs.find { |post| post.id == id }
+                    title = linked_post.data['title']
+                    content = linked_post.content
+                    "</p><a class='hover-link' #{$2}</a><div class='hover-card-container'><div class='hover-card'><strong class='hover-card-title'>#{title}</strong><br>#{content}</div></div><p>"
+                end
+            end
+        end
       end
     end
-  end
 end
