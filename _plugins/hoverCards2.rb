2fce3c105e244b42efb4c97e18b6cb865c93e612
Juan <jcgallegdup@gmail.com>
2024-03-12 11:24:19 -0700

    Wrap p elements in divs to re-introduce line-breaks

--- _plugins/hoverCards2.rb
+++ _plugins/hoverCards2.rb
@@ -8,15 +8,24 @@ module Jekyll
         # this prevents issues where linked_post.content for newer posts are still raw Markdown
         Jekyll::Hooks.register :site, :post_render do |site|
             site.posts.docs.each do |post|
-                post.output = post.output.gsub(/<a id="(.*?)" class="internal-site-link"(.*?)<\/a>/) do
-                    id = $1
-                    linked_post = site.posts.docs.find { |post| post.id == id }
-                    title = linked_post.data['title']
-                    content = linked_post.content
-                    "</p><a class='hover-link' #{$2}</a><div class='hover-card-container'><div class='hover-card'><strong class='hover-card-title'>#{title}</strong><br>#{content}</div></div><p>"
-                end
+                preserveParagraphs(post)
+                insertHoverCards(post, site)
             end
         end
-      end
     end
+
+    def preserveParagraphs(post)
+        post.output = post.output.gsub(/<p>(.*?)<\/p>/m, '<div class="paragraph"><p>\1</p></div>')
+    end
+
+    def insertHoverCards(post, site)
+        post.output = post.output.gsub(/<a id="(.*?)" class="internal-site-link"(.*?)<\/a>/) do
+          id = $1
+          linked_post = site.posts.docs.find { |post| post.id == id }
+          title = linked_post.data['title']
+          content = linked_post.content
+          "</p><a class='hover-link' #{$2}</a><div class='hover-card-container'><div class='hover-card'><strong class='hover-card-title'>#{title}</strong><br>#{content}</div></div><p>"
+        end
+    end
+  end
 end
