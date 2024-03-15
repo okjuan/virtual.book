dd1e49beb3d4032fb280a1225a91d45e6bcb863e
Juan <jcgallegdup@gmail.com>
2024-03-15 15:57:56 -0700

    Refactor hovercard position handler

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,14 +1,17 @@
 var observer = new IntersectionObserver(function(entries) {
-  entries.forEach(function(hoverCard) {
-    // If the element is visible and a hovercard
-    if (hoverCard.isIntersecting && hoverCard.target.classList.contains('hover-card')) {
+  entries.forEach(function(entry) {
+    if (!entry.target.classList.contains('hover-card')) {
+        return;
+    }
+    const hoverCard = entry.target;
+    if (hoverCard.style.display != 'none') {
       window.requestAnimationFrame(function() {
-        var rect = hoverCard.target.getBoundingClientRect();
+        var rect = hoverCard.getBoundingClientRect();
         var fullyInView = rect.top >= 0 && rect.left >= 0 && rect.bottom <= window.innerHeight && rect.right <= window.innerWidth;
         if (fullyInView) {
             return;
         }
-        const container = hoverCard.target.closest('.hover-card-container');
+        const container = hoverCard.closest('.hover-card-container');
         var correspondingHoverLink = container.parentElement.querySelector('.hover-link');
         var linkRect = correspondingHoverLink.getBoundingClientRect();
         var linkCenterX = linkRect.left + linkRect.width / 2;
@@ -20,17 +23,17 @@ var observer = new IntersectionObserver(function(entries) {
           : linkCenterY < viewportCenterY ? 'top right' : 'bottom right';
         if (linkQuadrant === 'top left') {
           // revert to default
-          hoverCard.target.style.top = '0px';
-          hoverCard.target.style.right = '0px;'
+          hoverCard.style.top = '0px';
+          hoverCard.style.right = '0px;'
         } else if (linkQuadrant === 'bottom left') {
-          hoverCard.target.style.top = '-' + (rect.height + linkRect.height) + 'px';
-          hoverCard.target.style.right = '0px;'
+          hoverCard.style.top = '-' + (rect.height + linkRect.height) + 'px';
+          hoverCard.style.right = '0px;'
         } else if (linkQuadrant === 'bottom right') {
-          hoverCard.target.style.top = '-' + (rect.height + linkRect.height) + 'px';
-          hoverCard.target.style.right = linkRect.width + 'px';
+          hoverCard.style.top = '-' + (rect.height + linkRect.height) + 'px';
+          hoverCard.style.right = linkRect.width + 'px';
         } else if (linkQuadrant === 'top right') {
-          hoverCard.target.style.top = '0px';
-          hoverCard.target.style.right = linkRect.width + 'px';
+          hoverCard.style.top = '0px';
+          hoverCard.style.right = linkRect.width + 'px';
         }
       });
     }
